0e382202873d14b6ac5f93f4dbbba9170166b8d1
mwmw1221 <mwzdalne@gmail.com>
2024-10-14 19:31:34 +0200

    Create meme-dictionary

---
+++ main.py
@@ -0,0 +1,11 @@
+meme_dict = {
+            "CRINGE": "Coś wyjątkowo dziwnego lub zawstydzającego",
+            "LOL": "Częsta reakcja na coś zabawnego",
+            }
+while True:
+    print("Podaj mem:")
+    ii = input("> ")
+    if ii in meme_dict:
+        print(meme_dict[i])
+    else:
+        print("Nie znaleziono.")
